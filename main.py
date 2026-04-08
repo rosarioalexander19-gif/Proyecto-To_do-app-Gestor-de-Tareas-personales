9bea310277b49fab18594073826140420c3ecfb6
fernandogerezp-spec <fernandogerezp@gmail.com>
2026-04-08 09:51:20 -0400

    Se agregó el archivo main.py
    Refactor imports and initialize services with repositories.

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-from proyecto_service import ProyectoService
-from ui import AppWindow
+from service.proyecto_service import ProyectoService
+from service.tarea_service import TareaService
+from ui.app_window import AppWindow
+from repository.Tarea_repository import proyectoRepository, tareaRepository
 
 def main():
     try:
-        # Inicializar servicio de proyectos
-        proyecto_service = ProyectoService()
+        # Inicializar servicio
+
+        proyecto_repo = proyectoRepository()
+        tarea_repo = tareaRepository()
+
+        proyecto_service = ProyectoService(proyecto_repo)
+        tarea_service = TareaService(proyecto_repo, tarea_repo)
+
 
         print("Sistema iniciado correctamente")
-        print("ProyectoService listo para usar")
+        
+        # Prueba básica de integración (opcional pero PRO)
+        proyectos = proyecto_service.obtener_proyectos()
+        print(f"Proyectos cargados: {len(proyectos)}")
 
-        # Integración con la interfaz gráfica (UI)
-        app = AppWindow(proyecto_service)
+        # Aquí luego se conectará la UI
+        # from ui import AppWindow
+        app = AppWindow(proyecto_service, tarea_service)
         app.run()
 
     except Exception as e:
         print("Error al iniciar el sistema:", e)
-
 
 if __name__ == "__main__":
     main()
